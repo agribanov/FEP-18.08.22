db2bfb424540468d37d3726ba9a4060c3d36887b
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2022-11-17 21:01:23 +0200

    node lection

---
+++ node-lection/app.js
@@ -0,0 +1,8 @@
+const calculator = require('./calculator');
+
+console.log(calculator.add(2, 3)); // 5
+console.log(calculator.sub(20, 3)); // 17
+console.log(calculator.mult(20, 3)); // 60
+console.log(calculator.div(21, 3)); // 7
+
+calculator | -add.js | -sub.js;
---
+++ node-lection/utils/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    RestApi: require('./RestApi'),
+    logger: require('./logger'),
+};
